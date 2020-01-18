3d19e4e285155e28e0e6749b6be2585636ed55ca
NicoleVenachi <ludwing@unicauca.edu.co>
2020-01-18 13:47:46 -0500

    Agregue elemento del capítulo de asincronismo

---
+++ Asincronimo/clase10 .js
@@ -0,0 +1,52 @@
+/* Múltiples request por cada personaje de la API */
+
+const API_URL = 'https://swapi.co/api/' //API API_URL
+const PEOPLE_URL = 'people/:id' // id de la persona a llamar en la API
+
+const opts = {crossDomain: true} // peticion a otra página
+
+//retorna promesa, no recibe callback como  parametro
+function obtenerPersonaje(id) {
+
+  return new Promise(function (resolve, reject) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, function (data) {
+      resolve(data) //cuando llegue la data resuelvo la promesa
+    })
+    .fail(() => reject(id))
+  })
+
+}
+
+function onError(id){
+  console.log(`Error al traer el personaje ${id}`)
+}
+
+async function obtenerPersonajes() {
+  var ids = [1, 2, 3, 4, 5, 6, 7]
+  var promesas = ids.map(id => obtenerPersonaje(id))
+  try {
+    var personajes = await Promise.all(promesas)
+    console.log(personajes)
+  }
+  catch(id) {
+    onError(id)
+  }
+}
+
+obtenerPersonajes()
+
+
+// obtenerPersonaje(1)
+//   .then(personaje => {
+//     console.log(`El personaje 1 es el ${personaje.name}`)
+//     return obtenerPersonaje(2)
+//   })
+//   .then(personaje2 => {
+//     console.log(`El personaje 2 es el ${personaje2.name}`)
+//     return obtenerPersonaje(3)
+//   })
+//   .then(personaje3 => {
+//     console.log(`El personaje 3 es el ${personaje3.name}`)
+//   })
+//   .catch(onError) //la puedo pasar por regerencia o invocar
